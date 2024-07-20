6e49f86aefc65bd525abced65f62d4e6aaa7f6e1
Manuel <manuelperezyebra@gmail.com>
2024-07-21 00:54:09 +0200

    Update giftExpertApp.styles.js

--- src/giftExpertApp/giftExpertApp.styles.js
+++ src/giftExpertApp/giftExpertApp.styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const MainContainer = styled.div`
   width: 80%;
   margin: 0px auto;
-  padding: 20px;
+  padding: 5px;
   @media only screen and (max-width: 425px) {
     width: 100%;
   }
