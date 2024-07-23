96236786e6acb7c7bca93f962c598302af5d38fa
Manuel <manuelperezyebra@gmail.com>
2024-07-23 16:19:44 +0200

    update

--- src/components/AddCategory/addCategory.styles.js
+++ src/components/AddCategory/addCategory.styles.js
@@ -3,36 +3,33 @@ import styled from 'styled-components';
 const StyledForm = styled.form`
   width: 100%;
   color: black;
+  align-items: center;
+  display: flex;
+
   input {
     color: #333333;
     font-size: 18px;
     border-radius: 10px;
-    padding: 10px;
-    padding-left: 20px;
     border: none;
     background-color: #e6e6e6;
     box-shadow: 0px 0px 32px -12px rgba(0, 0, 0, 0.15);
-    width: 50%;
-  }
-  input[type='submit'] {
-    margin-left: 1%;
-    font-size: 18px;
-    color: #333333;
-
-    width: 20%;
-
-    box-shadow: 0px 0px 32px -12px rgba(0, 0, 0, 0.15);
+    width: 60%;
+    padding: 10px;
   }
   input[type='number'] {
     color: #333333;
-
     margin-left: 1%;
-    font-size: 18px;
-
+    width: 18%;
+    text-align: center;
+    box-shadow: 0px 0px 32px -12px rgba(0, 0, 0, 0.15);
+  }
+  input[type='submit'] {
+    margin-left: 1%;
+    color: #333333;
     width: 20%;
     text-align: center;
-
     box-shadow: 0px 0px 32px -12px rgba(0, 0, 0, 0.15);
+    padding: 10px;
   }
   @media only screen and (max-width: 470px) {
     width: 100%;
@@ -42,7 +39,6 @@ const StyledForm = styled.form`
 
     input {
       font-size: 14px;
-
       border-radius: 10px;
       padding: 5px;
       border: none;
@@ -51,21 +47,20 @@ const StyledForm = styled.form`
       width: 60%;
     }
     input[type='number'] {
-      font-size: 14px;
-
       margin-left: 1%;
-      width: 20%;
-      text-align: center;
+      width: 18%;
+      padding: 5px;
 
+      text-align: center;
       box-shadow: 0px 0px 32px -12px rgba(0, 0, 0, 0.15);
     }
-  }
-  input[type='submit'] {
-    font-size: 14px;
+    input[type='submit'] {
+      margin-left: 1%;
+      padding: 5px;
 
-    margin-left: 1%;
-    width: 20%;
-    box-shadow: 0px 0px 32px -12px rgba(0, 0, 0, 0.15);
+      width: 20%;
+      box-shadow: 0px 0px 32px -12px rgba(0, 0, 0, 0.15);
+    }
   }
 `;
 
